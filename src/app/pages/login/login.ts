95d58e401728761f3735d9669978e20439ba852a
Ksanen <ksawiercz1222@gmail.com>
2025-09-20 15:03:57 +0200

    adding basic styling and creating a reactive form in login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
-
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export default class Login {}
+export default class Login {
+  form!: FormGroup;
+  wasValidated: boolean = false;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      login: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
+  logIn() {
+    this.wasValidated = true;
+    console.log(this.form);
+    if (this.form.invalid) return;
+  }
+}
