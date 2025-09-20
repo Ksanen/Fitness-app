2a5f951b1163c686af990e2d5c177c1810f6ba9e
Ksanen <ksawiercz1222@gmail.com>
2025-09-20 14:29:11 +0200

    adding routes and init login,register,notFoundPage pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login'),
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register'),
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./pages/not-found-page/not-found-page'),
+  },
+];
